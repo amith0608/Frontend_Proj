08e460047d866c9616336b7dd8af1f1e9b01de66
amith0608 <amith.sharma3@gmail.com>
2023-01-17 07:03:45 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,20 @@
+# Resume Builder 📄
+
+### A simple resume builder web app developed in react.
+
+### Technologies that are used in this project.
+
+  <ul>
+    <li>React</li> 
+    <li>Material UI, for UI</li>  
+    <li>JSpdf, for downloading resume.</li> 
+    <li>Redux, for state management.</li>  
+    <li>React-avatar-edit, for selecting profile image for resume.</li>
+    <li>React-router-dom, for routing.</li>
+  </ul>
+ 
+ #### Created By :    
+  <ul>
+    <li>Amith B N</li> 
+  </ul>
+
---
+++ src/Data/data.js
@@ -0,0 +1,37 @@
+export const data = {
+  personal_info: {
+    first_name: "Amith",
+    last_name: "BN",
+    email: "amith.sharma3@gmail.com",
+    mobile: "8197579935",
+    address: "201, V2 Vasundhara Apartment, Padmanabhanagar",
+    city: "Bengaluru",
+    state: "Karnataka",
+    postal_code: "560061",
+    objective:"Java, React, MySQL. ",
+  },
+  work_experience: [
+    {
+      id: 1,
+      job_title: "Full stack java developer",
+      organization_name: "Alma Better",
+      start_year: "2021",
+      end_year: "2022",
+    },
+    {
+      id: 2,
+      job_title: "FULL STACK WEB DEVELOPER",
+      organization_name: "Alma Better",
+      start_year: "2021",
+      end_year: "2022",
+    },
+  ],
+  education_details: {
+    type: "Graduation",
+    university: "VTU",
+    degree: "MCA",
+    start_year: "2013",
+    end_year: "2016",
+  },
+  key_skills: ["Java", "MySQL", "React"],
+};
